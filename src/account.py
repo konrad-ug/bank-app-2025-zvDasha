f448fb2902e6259d870879b23720dbc9cdcab18a
Daria Zverieva <d.zverieva.262@studms.ug.edu.pl>
2026-01-24 01:52:24 +0200

    history

--- src/account.py
+++ src/account.py
@@ -1,15 +1,17 @@
 class Account:
     def __init__(self, balance=0.0):
         self.balance = balance
+        self.history = [] 
 
     def incoming_transfer(self, amount):
         if amount > 0:
             self.balance += amount
+            self.history.append(amount)
 
     def outcoming_transfer(self, amount):
         if amount > 0 and amount <= self.balance:
             self.balance -= amount
-
+            self.history.append(-amount)
 
     def express_outgoing_transfer(self, amount):
         if amount <= 0:
@@ -18,9 +20,13 @@ class Account:
 
         if self.balance < amount:
             self.balance = -fee
+            self.history.append(-amount)  
+            self.history.append(-fee)    
             return
 
         self.balance -= (amount + fee)
+        self.history.append(-amount)        
+        self.history.append(-fee)         
     
 
     
